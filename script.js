a2eb0629a928142681462c6554ae6942f98e1c38
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-27 00:51:36 +0600

    nav and main section integration

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const logos = document.querySelectorAll(".logo")
 const logoContainer = document.querySelector('.logo-container')
 
+//nav functionality
+
 logos.forEach(logo => {
     logo.addEventListener('click', e => {
 
         const navActivateCondition = e.target.classList.contains('fa-bars');
+
         if (navActivateCondition) {
+
+            //nav icon animation - expand
             logoContainer.classList.remove("logo-inactive")
             logoContainer.classList.add("logo-active")
         } else {
+
+            //nav icon animation - contract
             logoContainer.classList.remove("logo-active")
             logoContainer.classList.add("logo-inactive")
         }
