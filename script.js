00984bb5fa5fefef5b3a09181de5b6422103102d
Salman Ahmed <salmanahmedx@hotmail.com>
2022-04-27 02:04:04 +0600

    final

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const logos = document.querySelectorAll(".logo")
 const logoContainer = document.querySelector('.logo-container')
 const nav = document.querySelector("nav")
+const body = document.querySelector(".body")
 
 //nav functionality
 
@@ -14,18 +15,26 @@ logos.forEach(logo => {
             //nav icon animation - expand
             logoContainer.classList.remove("logo-inactive")
             logoContainer.classList.add("logo-active")
+            logoContainer.classList.add("logo-container-active")
 
             //nav items
             nav.classList.toggle("nav-expand")
+
+            //body
+            body.classList.toggle('body-expand')
 
         } else {
 
             //nav icon animation - contract
             logoContainer.classList.remove("logo-active")
             logoContainer.classList.add("logo-inactive")
+            logoContainer.classList.remove("logo-container-active")
 
             //nav-items
             nav.classList.toggle("nav-expand")
+
+            //body
+            body.classList.toggle('body-expand')
         }
     })
 })
